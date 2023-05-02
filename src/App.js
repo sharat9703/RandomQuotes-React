1d37eecd80a26351aa7f0846ab3dd90ad5b88bf7
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 <sharath.yaragatti@yahoo.com>
2023-05-02 23:27:07 +0530

    work till setTheme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [quote,setQuote] = useState({});
+  const [theme,setTheme] = useState("");
   useEffect(()=>{
       const getData=async(f)=> {
       let x = await fetch(f);
@@ -10,13 +11,24 @@ function App() {
       console.log(y);
       setQuote(y);
     }
+    const changeTheme=()=>{
+      
+    let colors = ["red","yellow","green","blue","orange","brown"];
+    let color = colors[Math.floor(Math.random()*colors.length)];
+    
+    $(":root").style.setProperty('--mycolor',color);//need to add jquery later
+
+    }
     getData("https://randomquotes-zn0c.onrender.com/quotes");
+
 
   },{});
   return (
     <div className="App">
          <p>{quote.quote}</p>
          <p>{quote.author}</p>
+
+
     </div>
   );
 }
